25af4ca32d7f4e61b1efe9ff84205bc52ee68004
Michał Tyszkiewicz <michal.tyszkiewicz@gmail.com>
2024-02-19 19:27:17 +0100

    Restructure as a package

---
+++ src/coin_ai/losses.py
@@ -0,0 +1,57 @@
+import torch
+from torch import nn, Tensor
+
+class SigmoidLoss(nn.Module):
+    def forward(self, embeddings: Tensor, labels: Tensor) -> Tensor:
+        similarity = torch.einsum("ic,jc->ij", embeddings, embeddings)
+        gt_similarity = labels.unsqueeze(0) == labels.unsqueeze(1)
+        # ignore matching self-self
+        weight = 1 - torch.eye(
+            gt_similarity.size(0), dtype=torch.float, device=gt_similarity.device
+        )
+
+        return nn.functional.binary_cross_entropy_with_logits(
+            similarity, gt_similarity.float(), weight=weight, reduction="mean"
+        )
+
+    def similarity(self, embeddings: Tensor) -> Tensor:
+        return torch.einsum("ic,jc->ij", embeddings, embeddings).sigmoid()
+
+
+class DotProductLoss(nn.Module):
+    def forward(self, embeddings: Tensor, labels: Tensor) -> Tensor:
+        similarity = self.similarity(embeddings)
+        gt_similarity = labels.unsqueeze(0) == labels.unsqueeze(1)
+        # ignore matching self-self
+        weight = (~gt_similarity).float() * 2 - 1
+        weight[
+            torch.eye(gt_similarity.size(0), dtype=bool, device=gt_similarity.device)
+        ] = 0
+
+        return (weight * similarity).mean()
+
+    def similarity(self, embeddings: Tensor) -> Tensor:
+        embeddings = nn.functional.normalize(embeddings, dim=-1, p=2)
+        return torch.einsum("ic,jc->ij", embeddings, embeddings)
+
+
+class MarginLoss(nn.Module):
+    def forward(self, embeddings: Tensor, labels: Tensor) -> Tensor:
+        similarity = self.similarity(embeddings)
+        gt_similarity = labels.unsqueeze(0) == labels.unsqueeze(1)
+
+        positive_mask = gt_similarity & ~torch.eye(
+            gt_similarity.size(0), dtype=bool, device=gt_similarity.device
+        )
+        negative_mask = ~gt_similarity & ~torch.eye(
+            gt_similarity.size(0), dtype=bool, device=gt_similarity.device
+        )
+
+        positive = torch.where(positive_mask, similarity, torch.zeros_like(similarity))
+        negative = torch.where(negative_mask, similarity, torch.zeros_like(similarity))
+
+        return torch.relu(negative - positive + 0.5).mean()
+
+    def similarity(self, embeddings: Tensor) -> Tensor:
+        embeddings = nn.functional.normalize(embeddings, dim=-1, p=2)
+        return torch.einsum("ic,jc->ij", embeddings, embeddings)
---
+++ src/coin_ai/metrics.py
@@ -0,0 +1,28 @@
+from typing import Callable
+
+import torch
+from torch import nn, Tensor
+
+class AccuracyMetric(nn.Module):
+    def __init__(self, similarity: Callable[[Tensor], Tensor]):
+        super().__init__()
+        self.similarity = similarity
+
+    def forward(self, embeddings: Tensor, labels: Tensor) -> Tensor:
+        similarity = self.similarity(embeddings)
+        pad_values = torch.full(
+            (similarity.size(0),),
+            fill_value=-float("inf"),
+            dtype=similarity.dtype,
+            device=similarity.device,
+        )
+        similarity = torch.diagonal_scatter(similarity, pad_values)
+        gt_similarity = labels.unsqueeze(0) == labels.unsqueeze(1)
+
+        _values, indices = similarity.topk(10, dim=1)
+        is_correct_at_k = gt_similarity[
+            torch.arange(similarity.size(0)).unsqueeze(1), indices
+        ]
+        is_correct_up_to_k = is_correct_at_k.cumsum(dim=1).bool()
+
+        return is_correct_up_to_k.float().mean(dim=0)

a86c9d543aa9aae614761c4262ced8001e85d1e6
Michał Tyszkiewicz <michal.tyszkiewicz@gmail.com>
2024-06-30 11:50:06 +0200

    Infer homography relations via graph traversal

--- src/coin_ai/alignment/data.py
+++ src/coin_ai/alignment/data.py
@@ -3,74 +3,229 @@ from __future__ import annotations
 import csv
 import os
 from dataclasses import dataclass
+from functools import partial
+from typing import NamedTuple
 
+import torch
 import imageio
-import numpy as np
-import cv2
+import networkx as nx
+import kornia.geometry as KG
+from torch import Tensor
+from einops import rearrange, repeat
 
 
 @dataclass
-class HPair:
-    WARP_KWARGS = dict(dsize=(518, 518), borderMode=cv2.BORDER_REPLICATE)
+class HPathPair:
+    path1: str
+    path2: str
+    H_12: Tensor
 
-    image1: np.ndarray
-    image2: np.ndarray
-    H_12: np.ndarray
+    def flip(self) -> HPathPair:
+        return HPathPair(self.path2, self.path1, self.H_12.inverse())
 
     @staticmethod
-    def from_paths(path1: str, path2: str, H_12: np.ndarray) -> HPair:
-        return HPair(
-            imageio.imread(path1),
-            imageio.imread(path2),
-            H_12,
-        )
+    def _load_image(path: str) -> Tensor:
+        return torch.tensor(imageio.imread(path)).float() / 255.0
+
+    def get_image1(self) -> Tensor:
+        return self._load_image(self.path1)
+
+    def get_image2(self) -> Tensor:
+        return self._load_image(self.path2)
+
+
+class HomographyBatch(NamedTuple):
+    images: Tensor  # [2, B, C, H, W]
+    H_12: Tensor  # [B, 3, 3]
+
+    def __repr__(self) -> str:
+        return f"HomographyBatch(images={tuple(self.images.shape)}, H_12={tuple(self.H_12.shape)})"
+
+    @staticmethod
+    def from_path_pairs(pairs: list[HPathPair]) -> HomographyBatch:
+        images = [torch.stack([p.get_image1(), p.get_image2()], dim=0) for p in pairs]
+        H_12s = [pair.H_12 for pair in pairs]
+
+        images = torch.stack(images, dim=1)
+        images = rearrange(images, "t b h w c -> t b c h w")
+        H_12s = torch.stack(H_12s)
+
+        return HomographyBatch(images=images, H_12=H_12s)
+
+    def apply_h(self, H_12: Tensor) -> HomographyBatch:
+        H_12 = H_12.to(self.images.device)
+        images = KG.homography_warp(self.images, H_12)
+        return HomographyBatch(images=images, H_12=self.H_12)
+
+    def slice(self, start: int | None, end: int | None) -> HomographyBatch:
+        images = self.images[:, start:end]
+        H_12 = self.H_12[start:end]
+
+        return HomographyBatch(images=images, H_12=H_12)
 
     @property
-    def H_21(self) -> np.ndarray:
-        return np.linalg.inv(self.H_12)
+    def B(self) -> int:
+        return self.H_12.shape[0]
 
-    def image1_warped(self) -> np.ndarray:
-        return cv2.warpPerspective(self.image1, self.H_12, **self.WARP_KWARGS)
+    @property
+    def corners(self) -> Tensor:
+        h, w = self.images.shape[-2:]
 
-    def image2_warped(self) -> np.ndarray:
-        return cv2.warpPerspective(self.image2, self.H_21, **self.WARP_KWARGS)
+        return torch.tensor([[0, 0], [w, 0], [w, h], [0, h]], dtype=torch.float32)
+
+    def get_alignment_transform(self) -> Tensor:
+        return torch.stack(
+            [
+                self.H_12,
+                repeat(torch.eye(3), "i j -> b i j", b=self.B),
+            ]
+        )
+
+    def build_augmentation(self) -> AugmentationBuilder:
+        return AugmentationBuilder(
+            batch=self,
+            transform=repeat(torch.eye(3), "i j -> c b i j", c=2, b=self.B),
+            target_size=self.images.shape[-2:],
+        )
 
 
-def parse_homography_csv(source_path: str) -> list[HPair]:
-    HEADERS = ["img1", "h11", "h12", "h13", "h21", "h22", "h23", "h31", "h32", "h33"]
+class AugmentationBuilder(NamedTuple):
+    batch: HomographyBatch
+    transform: Tensor  # [2, B, 3, 3]
+    target_size: tuple[int, int]
 
-    if not os.path.exists(source_path):
-        raise ValueError(f"File {source_path} does not exist")
+    def resize(self, target_size: tuple[int, int]) -> AugmentationBuilder:
+        h, w = target_size
+        resize_transform = KG.get_perspective_transform(
+            self.batch.corners.unsqueeze(0),
+            torch.tensor(
+                [[0, 0], [w, 0], [w, h], [0, h]], dtype=torch.float32
+            ).unsqueeze(0),
+        )
+        resize_transform = repeat(resize_transform, "B i j -> C B i j", C=2)
 
-    pairs = []
-    with open(source_path, "r") as f:
-        reader = csv.reader(f)
-        headers = next(reader)
-        assert headers[: len(HEADERS)] == HEADERS
+        return AugmentationBuilder(
+            batch=self.batch,
+            transform=resize_transform @ self.transform,
+            target_size=target_size,
+        )
 
-        for img1, img2, *floats in reader:
-            H_floats = floats[:9]
-            source_dir = os.path.dirname(source_path)
-            path1 = os.path.join(source_dir, img1)
-            path2 = os.path.join(source_dir, img2)
-            H_opencv = np.array(H_floats, dtype=np.float32).reshape(3, 3)
-            pairs.append(
-                HPair.from_paths(
-                    path1,
-                    path2,
-                    H_opencv,
+    def get_random_homography_transform(self, scale: float = 0.125) -> Tensor:
+        corners = repeat(self.batch.corners, "i j -> b i j", b=self.batch.B)
+
+        scale_px = torch.tensor([self.target_size]).reshape(1, 1, 2) * scale
+        corners_1 = corners + torch.randn_like(corners) * scale_px
+        corners_2 = corners + torch.randn_like(corners) * scale_px
+        transform_1 = KG.get_perspective_transform(corners_1, corners_2)
+        transform_2 = KG.get_perspective_transform(corners_2, corners_1)
+
+        return torch.stack([transform_2, transform_1], dim=0)
+
+    def apply(self, transform: Tensor) -> AugmentationBuilder:
+        assert transform.shape == self.transform.shape
+        t_1, t_2 = transform
+        new_H_12 = t_2 @ self.batch.H_12 @ torch.inverse(t_1)
+        return AugmentationBuilder(
+            batch=self.batch._replace(H_12=new_H_12),
+            transform=transform @ self.transform,
+            target_size=self.target_size,
+        )
+
+    def build(self) -> HomographyBatch:
+        warp_fn = partial(
+            KG.warp_perspective, dsize=self.target_size, padding_mode="border"
+        )
+        return HomographyBatch(
+            images=torch.vmap(warp_fn, in_dims=(0, 0))(
+                self.batch.images, self.transform
+            ),
+            H_12=self.batch.H_12,
+        )
+
+
+class HPairDataset:
+    def __init__(self, csv_path: str):
+        self.base_pairs = self.parse_homography_csv(csv_path)
+        self.inferred_pairs = self.infer_homographies(self.base_pairs)
+
+    def __len__(self) -> int:
+        return len(self.inferred_pairs)
+
+    def __getitem__(self, idx: int) -> HomographyBatch:
+        return HomographyBatch.from_path_pairs([self.inferred_pairs[idx]])
+
+    @staticmethod
+    def parse_homography_csv(source_path: str) -> list[HPathPair]:
+        HEADERS = [
+            "img1",
+            "h11",
+            "h12",
+            "h13",
+            "h21",
+            "h22",
+            "h23",
+            "h31",
+            "h32",
+            "h33",
+        ]
+
+        if not os.path.exists(source_path):
+            raise ValueError(f"File {source_path} does not exist")
+
+        pairs = []
+        with open(source_path, "r") as f:
+            reader = csv.reader(f)
+            headers = next(reader)
+            assert headers[: len(HEADERS)] == HEADERS
+
+            for img1, img2, *floats in reader:
+                H_floats = [float(f) for f in floats[:9]]
+                source_dir = os.path.dirname(source_path)
+                path1 = os.path.join(source_dir, img1)
+                path2 = os.path.join(source_dir, img2)
+                H_opencv = torch.tensor(H_floats, dtype=torch.float32).reshape(3, 3)
+                pairs.append(
+                    HPathPair(
+                        path1,
+                        path2,
+                        H_opencv.inverse(),
+                    )
                 )
-            )
 
-    return pairs
+        return pairs
 
+    @staticmethod
+    def infer_homographies(hpairs: list[HPathPair]) -> list[HPathPair]:
+        g = nx.DiGraph()
+        for pair in hpairs + [pair.flip() for pair in hpairs]:
+            g.add_edge(pair.path1, pair.path2, H_matrix=pair.H_12)
 
-def recursive_parse_homography_csv(root_path: str) -> list[HPair]:
-    parsed = []
+        inferred_pairs = []
+        for source, paths in nx.all_pairs_shortest_path(g):
+            for target, path in paths.items():
+                path_graph = nx.path_graph(path)
 
-    for root, _, files in os.walk(root_path):
-        for file in files:
-            if file == "homographies.csv":
-                parsed.extend(parse_homography_csv(os.path.join(root, file)))
+                total_h_matrix = torch.eye(3)
+                for s, t in path_graph.edges():
+                    h_matrix = g.edges[s, t]["H_matrix"]
+                    total_h_matrix = total_h_matrix @ h_matrix
 
-    return parsed
+                inferred_pairs.append(
+                    HPathPair(
+                        target,
+                        source,
+                        total_h_matrix,
+                    )
+                )
+
+        return inferred_pairs
+
+    # def recursive_parse_homography_csv(root_path: str) -> list[HPair]:
+    #    parsed = []
+
+    #    for root, _, files in os.walk(root_path):
+    #        for file in files:
+    #            if file == "homographies.csv":
+    #                parsed.extend(parse_homography_csv(os.path.join(root, file)))
+
+    #    return parsed
